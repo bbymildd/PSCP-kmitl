23fc212e84ca19199ca64643797848c52f9e8612
Nutpimol <npm.mildy@gmail.com>
2026-07-05 22:32:01 +0700

    feat: add code

--- week1/matheuclid.py
+++ week1/matheuclid.py
@@ -1,9 +1,13 @@
 """math"""
 def main():
     """math"""
-    q1 = int(input())
-    q2 = int(input())
-    p1 = int(input())
-    p2 = int(input())
+    q1 = float(input())
+    q2 = float(input())
+    p1 = float(input())
+    p2 = float(input())
 
-    
+    d =(((q1-p1)**2) + ((q2-p2)**2))**(1/2)
+
+    print(d)
+
+main()
